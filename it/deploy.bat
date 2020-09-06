d8cfa36fffa3e6bbd9af4747ba95942f69cad8b1
Евгений Альтман <altmanea@gmail.com>
2020-09-06 21:02:14 +0600

    add deloy bats

---
+++ it/deploy.bat
@@ -0,0 +1,7 @@
+hugo
+
+SET COURSE=it
+
+ROBOCOPY "C:\work\courses\%COURSE%\public"   "C:\Work\AltmanEA.github.io\%COURSE%"
+
+RD /S /Q "C:\work\courses\%COURSE%\public\"
---
+++ oop/deploy.bat
@@ -0,0 +1,7 @@
+hugo
+
+SET COURSE=oop
+
+ROBOCOPY "C:\work\courses\%COURSE%\public"   "C:\Work\AltmanEA.github.io\%COURSE%"
+
+RD /S /Q "C:\work\courses\%COURSE%\public\"
