a33e4e03b75ce6987388df12e131905ae56d35cc
Евгений Альтман <altmanea@gmail.com>
2020-09-09 10:26:17 +0600

    fix subdir deploy

--- oop/deploy.bat
+++ oop/deploy.bat
@@ -2,6 +2,6 @@ hugo
 
 SET COURSE=oop
 
-ROBOCOPY "C:\work\courses\%COURSE%\public"   "C:\Work\AltmanEA.github.io\%COURSE%"
+ROBOCOPY "C:\work\courses\%COURSE%\public"   "C:\Work\AltmanEA.github.io\%COURSE%" /S
 
 RD /S /Q "C:\work\courses\%COURSE%\public\"
