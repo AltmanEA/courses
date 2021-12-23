16dde534b3f4cd9ad8964ad6319147dd164dba60
Евгений Альтман <altmanea@gmail.com>
2021-12-23 08:56:46 +0600

    update exam

--- oop/deploy.bat
+++ oop/deploy.bat
@@ -3,6 +3,6 @@ hugo
 SET COURSE=oop
 SET CURDRIVE=%CD:~0,3%
 
-ROBOCOPY "%CURDRIVE%work\courses\%COURSE%\public"   "%CURDRIVE%Work\AltmanEA.github.io\%COURSE%" /S
+ROBOCOPY "%CURDRIVE%work\edu\courses\%COURSE%\public"   "%CURDRIVE%Work\AltmanEA.github.io\%COURSE%" /S
 
-RD /S /Q "%CURDRIVE%work\courses\%COURSE%\public\"
+RD /S /Q "%CURDRIVE%work\edu\courses\%COURSE%\public\"
